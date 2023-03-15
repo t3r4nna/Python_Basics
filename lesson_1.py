d44a13b1818e0f82a92b7f7fec762edd094cb712
Anna <t3r4nna@gmail.com>
2023-03-15 14:40:06 +0300

    Create lesson_1.py

---
+++ lesson_1.py
@@ -0,0 +1,14 @@
+sec = int(input("Введите время в секундах"))
+
+
+def what_time(sec):
+    import datetime
+    hours = sec // 3600
+    minutes = (sec - hours*3600) // 60
+    seconds = sec - hours*3600 - minutes*60
+    if sec < 3600:
+        hours = 0
+    if sec < 60:
+        minutes = 0
+    result_time = datetime.time(hours, minutes, seconds)
+    return f"Введенное время: {result_time}"
